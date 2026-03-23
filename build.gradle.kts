b3fb016dc395858cf485bf4002a9a8f662043701
Yannick <vankerckvoordeyannick@gmail.com>
2026-03-23 17:24:50 +0100

    Added database dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val hikari_version: String by project
+val postgres_version: String by project
 
 plugins {
     kotlin("jvm") version "2.3.0"
@@ -27,6 +30,13 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
+    //Database
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("com.zaxxer:HikariCP:$hikari_version")
+    implementation("org.postgresql:postgresql:$postgres_version")
+    // Test
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
