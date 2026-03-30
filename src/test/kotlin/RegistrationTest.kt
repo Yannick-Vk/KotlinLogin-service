f3409a2c81362a1f26c54e62a8f33aeff04397ac
Yannick <vankerckvoordeyannick@gmail.com>
2026-03-30 16:06:45 +0200

    Added empty username request

--- src/test/kotlin/RegistrationTest.kt
+++ src/test/kotlin/RegistrationTest.kt
@@ -4,7 +4,6 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.json
-import io.ktor.server.config.*
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation as ClientContentNegotiation
 import io.ktor.server.testing.*
 import xyz.mitzie.dto.RegisterUserRequest
@@ -14,18 +13,17 @@ import kotlin.test.assertTrue
 
 class RegistrationTest {
 
-    private val valid_username = "newUser"
-    private val valid_email = "newuser@example.com"
-    private val valid_password = "newPassword"
+    private val validUsername = "newUser"
+    private val validEmail = "newuser@example.com"
+    private val validPassword = "newPassword"
+
+    // Requests
+    private val emptyUsernameRequest = RegisterUserRequest("", validEmail, validPassword)
 
     @Test
     fun testRegistrationSuccess() = testApplication {
-        environment {
-            config = testConfig
-        }
-        application {
-            module()
-        }
+        environment { config = testConfig }
+        application { module() }
 
         val client = createClient {
             install(ClientContentNegotiation) {
@@ -33,7 +31,7 @@ class RegistrationTest {
             }
         }
 
-        val requestBody = RegisterUserRequest(valid_username, valid_email, valid_password)
+        val requestBody = RegisterUserRequest(validUsername, validEmail, validPassword)
 
         client.post("/register") {
             contentType(ContentType.Application.Json)
@@ -41,9 +39,31 @@ class RegistrationTest {
         }.apply {
             assertEquals(HttpStatusCode.Created, status)
             val responseBody = bodyAsText()
-            assertTrue(responseBody.contains(valid_username))
-            assertTrue(responseBody.contains(valid_email))
+            assertTrue(responseBody.contains(validUsername))
+            assertTrue(responseBody.contains(validEmail))
         }
+
+    }
+
+    @Test
+    fun testRegisterWithEmptyUsername() = testApplication {
+        environment { config = testConfig }
+        application { module() }
+
+        val client = createClient {
+            install(ClientContentNegotiation) {
+                json()
+            }
+        }
+
+        client.post("/register") {
+            contentType(ContentType.Application.Json)
+            setBody(emptyUsernameRequest)
+        }.apply {
+            assertEquals(HttpStatusCode.BadRequest, status)
+            assertEquals("Username cannot be empty", bodyAsText())
+        }
+
 
     }
 }
