0f11db473dde8c9450e8b7ae5e213fead93c2048
Yannick <vankerckvoordeyannick@gmail.com>
2026-03-31 19:55:43 +0200

    Some simple login tests (with user registration)

---
+++ src/test/kotlin/LoginTest.kt
@@ -0,0 +1,151 @@
+package xyz.mitzie
+
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.testing.*
+import xyz.mitzie.dto.LoginUserRequest
+import xyz.mitzie.dto.RegisterUserRequest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation as ClientContentNegotiation
+
+class LoginTest {
+
+    private val validUsername = "newUser"
+    private val validEmail = "newuser@example.com"
+    private val validPassword = "newPassword"
+
+    // Requests
+    private val emptyUsernameRequest = LoginUserRequest("", validPassword)
+    private val emptyPasswordRequest = LoginUserRequest(validUsername, "")
+
+    private val route = "/login"
+    private val registerRoute = "/register"
+
+
+
+    suspend fun registerUser(client: HttpClient) {
+        val registerBody = RegisterUserRequest(validUsername, validEmail, validPassword)
+
+        client.post(registerRoute) {
+            contentType(ContentType.Application.Json)
+            setBody(registerBody)
+        }
+    }
+
+    @Test
+    fun testRegisterWithEmptyUsername() = testApplication {
+        environment { config = testConfig }
+        application { module() }
+
+        val client = createClient {
+            install(ClientContentNegotiation) {
+                json()
+            }
+        }
+
+        client.post(route) {
+            contentType(ContentType.Application.Json)
+            setBody(emptyUsernameRequest)
+        }.apply {
+            assertEquals(HttpStatusCode.BadRequest, status)
+            assertEquals("Username cannot be empty", bodyAsText())
+        }
+    }
+
+    @Test
+    fun testRegisterWithEmptyPassword() = testApplication {
+        environment { config = testConfig }
+        application { module() }
+
+        val client = createClient {
+            install(ClientContentNegotiation) {
+                json()
+            }
+        }
+
+        client.post(route) {
+            contentType(ContentType.Application.Json)
+            setBody(emptyPasswordRequest)
+        }.apply {
+            assertEquals(HttpStatusCode.BadRequest, status)
+            assertEquals("Password cannot be empty", bodyAsText())
+        }
+    }
+
+
+    @Test
+    fun testLoginSuccess() = testApplication {
+        environment { config = testConfig }
+        application { module() }
+
+        val client = createClient {
+            install(ClientContentNegotiation) {
+                json()
+            }
+        }
+
+        registerUser(client)
+
+        val loginBody = LoginUserRequest(validUsername, validPassword)
+
+        client.post(route) {
+            contentType(ContentType.Application.Json)
+            setBody(loginBody)
+        }.apply {
+            assertEquals(HttpStatusCode.OK, status)
+        }
+
+    }
+
+    @Test
+    fun testLoginUserNotFound() = testApplication {
+        environment { config = testConfig }
+        application { module() }
+
+        val client = createClient {
+            install(ClientContentNegotiation) {
+                json()
+            }
+        }
+
+        registerUser(client)
+
+        val loginBody = LoginUserRequest("unknown_username", "unknown password")
+
+        client.post(route) {
+            contentType(ContentType.Application.Json)
+            setBody(loginBody)
+        }.apply {
+            assertEquals(HttpStatusCode.BadRequest, status)
+        }
+
+    }
+
+    @Test
+    fun testLoginWrongPassword() = testApplication {
+        environment { config = testConfig }
+        application { module() }
+
+        val client = createClient {
+            install(ClientContentNegotiation) {
+                json()
+            }
+        }
+
+        registerUser(client)
+
+        val loginBody = LoginUserRequest(validUsername, "wrong password")
+
+        client.post(route) {
+            contentType(ContentType.Application.Json)
+            setBody(loginBody)
+        }.apply {
+            assertEquals(HttpStatusCode.BadRequest, status)
+        }
+
+    }
+}
