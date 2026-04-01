f38f62c116757200b4a19d8b7f19b24aa6f5e39f
Yannick <vankerckvoordeyannick@gmail.com>
2026-04-01 14:49:47 +0200

    Setup base table for password reset tokens

---
+++ src/main/kotlin/models/ResetPasswordTable.kt
@@ -0,0 +1,11 @@
+package xyz.mitzie.models
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
+
+object ResetPasswordTable : IntIdTable("reset_passwords") {
+    val userid = reference("user_id", UsersTable.id, ReferenceOption.CASCADE)
+    val tokenHash = varchar("token_hash", 255).uniqueIndex()
+    val expiresAt =  varchar("expires_at", 50)
+    val used = bool("used")
+}
