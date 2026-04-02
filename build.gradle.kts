8eb6139628f9bab44527fe4a13d2faacee979f97
Yannick <vankerckvoordeyannick@gmail.com>
2026-04-02 19:28:18 +0200

    Updated expiresAt type to datetime

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposed_version")
     implementation("com.zaxxer:HikariCP:$hikari_version")
     implementation("org.postgresql:postgresql:$postgres_version")
     implementation("org.mindrot:jbcrypt:0.4")
--- src/main/kotlin/models/ResetPasswordTable.kt
+++ src/main/kotlin/models/ResetPasswordTable.kt
@@ -1,11 +1,12 @@
 package xyz.mitzie.models
 
+import org.jetbrains.exposed.sql.kotlin.datetime.datetime
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.ReferenceOption
 
 object ResetPasswordTable : IntIdTable("reset_passwords") {
     val userid = reference("user_id", UsersTable.id, ReferenceOption.CASCADE)
     val tokenHash = varchar("token_hash", 255).uniqueIndex()
-    val expiresAt =  varchar("expires_at", 50)
+    val expiresAt = datetime("expires_at")
     val used = bool("used")
 }
