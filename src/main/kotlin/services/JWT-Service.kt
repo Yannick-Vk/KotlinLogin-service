50615b15b599826f03523638db48ed72e352b14e
Yannick <vankerckvoordeyannick@gmail.com>
2026-04-02 20:14:30 +0200

    Created helper function to generate both tokens

--- src/main/kotlin/services/JWT-Service.kt
+++ src/main/kotlin/services/JWT-Service.kt
@@ -2,26 +2,30 @@ package xyz.mitzie.services
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import io.ktor.server.application.ApplicationCall
-import io.ktor.server.auth.jwt.JWTPrincipal
-import io.ktor.server.auth.principal
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import xyz.mitzie.JwtConfig
 import xyz.mitzie.dto.TokenResponse
 import xyz.mitzie.dto.UserClaim
 import xyz.mitzie.dto.UserDTO
-import java.util.Date
+import java.util.*
 
 fun calculateExpiration(tokenLifetimeMs: Long): Date {
     // Set token expiration time in ms
     return Date(System.currentTimeMillis() + tokenLifetimeMs)
 }
 
-fun generateAccessToken(jwtConfig: JwtConfig, user: UserDTO) :String {
-    return generateGenericToken(jwtConfig, user, jwtConfig.accessTokenExpiration);
+fun generateAccessToken(jwtConfig: JwtConfig, user: UserDTO): String {
+    return generateGenericToken(jwtConfig, user, jwtConfig.accessTokenExpiration)
 }
 
-fun generateRefreshToken(jwtConfig: JwtConfig, user: UserDTO) :String {
-    return generateGenericToken(jwtConfig, user, jwtConfig.refreshTokenExpiration);
+fun generateRefreshToken(jwtConfig: JwtConfig, user: UserDTO): String {
+    return generateGenericToken(jwtConfig, user, jwtConfig.refreshTokenExpiration)
+}
+
+fun generateFullToken(jwtConfig: JwtConfig, user: UserDTO): TokenResponse {
+    return TokenResponse(generateAccessToken(jwtConfig, user), generateRefreshToken(jwtConfig, user))
 }
 
 private fun generateGenericToken(jwtConfig: JwtConfig, user: UserDTO, lifetime: Long): String {
@@ -38,11 +42,11 @@ private fun generateGenericToken(jwtConfig: JwtConfig, user: UserDTO, lifetime: 
     return token
 }
 
-fun getUserFromToken(call: ApplicationCall) : UserClaim {
+fun getUserFromToken(call: ApplicationCall): UserClaim {
     val principal = call.principal<JWTPrincipal>()
     // Get username from token claim
     val username = principal!!.payload.getClaim("username").asString()
-    val email= principal.payload.getClaim("email").asString()
+    val email = principal.payload.getClaim("email").asString()
     val expiresAt = principal.payload.getClaim("exp").asLong()
 
     return UserClaim(username, email, expiresAt)
